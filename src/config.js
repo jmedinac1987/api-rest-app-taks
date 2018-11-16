f305a19027132fb0e91526738ac9f6a5fcbef8f7
Jorge Andres Medina Castro <jmedinac1987@gmail.com>
2018-11-16 03:35:32 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
   db: 'mongodb://jmedinac1987:Yisela1987@ds159772.mlab.com:59772/tasks',
   optionsDB: { useNewUrlParser: true },
   email: 'infojorgemedinawebsite@gmail.com',
-  key: 'bRy$J3025*D3$aFik*E',
+  key: 'by$J3025*D3$aFik*E',
   SECRET_TOKEN: '$uNicat20li128YY99nt$$',
   corsOptions: {    
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
